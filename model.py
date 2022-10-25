7119354c6212a51b542311cb8741d435da39808f
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-10-25 11:02:32 +0300

    changed relationships

--- model.py
+++ model.py
@@ -5,14 +5,17 @@ from sqlalchemy.orm import relationship
 engine = create_engine('sqlite:///data/mokyklos_studentai.db')
 Base = declarative_base()
 
+
+
 class Mokykla(Base):
     __tablename__ = "mokykla"
     id = Column(Integer, primary_key=True)
     pavadinimas = Column("pavadinimas", String)
     imones_kodas = Column("imones_kodas", Integer, unique=True, nullable=False)
-    pvm_kodas = Column("pvm_kodas", String, unique=True, nullable=True)
+    pvm_kodas = Column("pvm_kodas", String)
     adresas = Column("adresas", String)
-    studentai = relationship("Studentas", back_populates="mokyklos")
+    # studentai = relationship("Studentas", back_populates="mokyklos")
+    studijos = relationship("Studijos", back_populates="mokykla")
 
     def __repr__(self):
         return f"{self.id}, {self.pavadinimas}, {self.imones_kodas}, {self.pvm_kodas}, {self.adresas}"
@@ -24,10 +27,11 @@ class Studijos(Base):
     trukme = Column("trukme", String)
     kaina = Column("kaina", Float)
     mokykla_id = Column("mokykla_id", Integer, ForeignKey("mokykla.id"))
+    mokykla = relationship("Mokykla", back_populates="studijos")
     studentai = relationship("Studentas", back_populates="studijos")
 
     def __repr__(self):
-        return f"{self.id}, {self.programos_pavadinimas}, {self.trukme}, {self.kaina}"
+        return f"{self.id}, {self.programos_pavadinimas}, {self.trukme}, {self.kaina}, {self.mokykla}"
 
 class Studentas(Base):
     __tablename__ = "studentas"
@@ -37,8 +41,6 @@ class Studentas(Base):
     asm_kodas = Column("asmens_kodas", Integer, unique=True, nullable=False)
     el_pastas = Column("el_pastas", String)
     mobilus = Column("tel_nr", Integer)
-    mokykla_id = Column("mokykla_id", Integer, ForeignKey("mokykla.id"))
-    mokyklos = relationship("Mokykla", back_populates="studentai")
     studijos_id = Column("studijos_id", Integer, ForeignKey("studijos.id"))
     studijos = relationship("Studijos", back_populates="studentai")
 
@@ -47,5 +49,5 @@ class Studentas(Base):
 
 
 if __name__ == "__main__":
-    # Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
